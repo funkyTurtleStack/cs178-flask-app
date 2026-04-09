fb9dc20aa6fa79422d5b88802dce2a67ca987f02
happy-turtle-stack <braedon.stapelman@drake.edu>
2026-04-09 15:02:13 -0500

    set up skeleton for all routes and commented out the example routes

--- flaskapp.py
+++ flaskapp.py
@@ -10,11 +10,85 @@ from flask import Flask
 from flask import render_template
 from flask import Flask, render_template, request, redirect, url_for, flash
 from dbCodeRDS import *
+from dbCodeDynamo import*
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key' # this is an artifact for using flash displays; 
                                    # it is required, but you can leave this alone
 
+####### PAGE ROUTES #######
+@app.route('/')
+def landingPage():
+    '''Brings user to the landing page and redirects if they are signed in'''
+
+@app.route('/SignInPage', methods=['GET'])
+def signInPage():
+    '''Brings user to the signin page'''
+
+@app.route('/SignUpPage', methods=['GET'])
+def signUpPage():
+    '''Brings user to the sign up page'''
+
+@app.route('/HomePage', methods=['GET'])
+def homePage():
+    '''Brings user to the home page'''
+
+@app.route('/UserPage', methods=['GET'])
+def userPage():
+    '''Brings user to user page'''
+
+@app.route('/CheckoutPage', methods=['GET'])
+def checkoutPage():
+    '''Brings user ot checkout page'''
+
+
+####### Action Routes #######
+##-!-!- No Encryption Yet -!-!-##
+
+@app.route('/SignIn', methods=['POST'])
+def signIn():
+    '''Signs in user and creates session'''
+
+@app.route('/SignOut', methods=['POST'])
+def signOut():
+    '''Signs out user and deletes session'''
+
+@app.route('/CreateUser', methods=['POST'])
+def createUser():
+    '''Creates a new user (email must be unique)'''
+
+@app.route('/ChangeUsername', methods=['PUT'])
+def changeUsername():
+    '''Changes a user's username'''
+    #remember to incorporate flash message
+    #remember to make sure the user can only change their own
+
+@app.route('/ChangePassword', methods=['PUT'])
+def changePassword():
+    '''Changes a user's password'''
+    #remember to incorporate flash message
+    #remember to make sure the user can only change their own
+
+@app.route('/ChangeEmail', methods=['PUT'])
+def changeEmail():
+    '''Changes a user's email'''
+    #remember to incorporate flash message
+    #remember to make sure the user can only change their own
+
+@app.route('/DeleteAccount', methods=['DELETE'])
+def deleteAccount():
+    '''Deletes a user's account'''
+    #remember to incorporate flash message
+    #remember to make sure the user can only delete their own
+
+@app.route('/MakePurchase', methods=['PUT'])
+def makePurchase():
+    '''Reduces the stock of items in the database based on how many of each item the user has in their cart'''
+
+
+
+
+'''
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -86,7 +160,7 @@ def viewdb():
 
 """==============================================================="""
 
-
+'''
 
 # these two lines of code should always be the last in the file
 if __name__ == '__main__':
